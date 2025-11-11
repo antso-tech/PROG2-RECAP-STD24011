c8f635d5ad8010b2dbad229da85a7b9662ba8fc4
antso.tech <hei.antso.2007@gmail.com>
2025-11-11 06:40:40 +0300

    ajout des constructeurs avec getter

--- src/main/java/org/example/Exams.java
+++ src/main/java/org/example/Exams.java
@@ -1,13 +1,23 @@
 package org.example;
 
+import lombok.Getter;
 import java.time.Instant;
 
+@Getter
 public class Exams {
     int id;
     String title;
     Subject subject;
     Instant date;
     int coefficient;
+
+    public Exams(int id, String title, Subject subject, Instant date, int coefficient) {
+        this.id = id;
+        this.title = title;
+        this.subject = subject;
+        this.date = date;
+        this.coefficient = coefficient;
+    }
 
 
 }
