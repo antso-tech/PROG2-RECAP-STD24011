968f56db82c67cbddf5ddedd3ab106c375cb6e03
antso.tech <hei.antso.2007@gmail.com>
2025-11-11 07:04:44 +0300

    nouvel ajout des attributs avec private et final

--- src/main/java/org/example/Teacher.java
+++ src/main/java/org/example/Teacher.java
@@ -1,14 +1,26 @@
 package org.example;
 
+import lombok.Getter;
+
 import java.time.LocalDate;
 
+@Getter
 public class Teacher {
-    int id;
-    String firstName;
-    String lastName;
-    LocalDate birthdate;
-    String email;
-    String phoneNumber;
-    String Speciality ;
+    private final int id;
+    private final String firstName;
+    private final String lastName;
+    private final LocalDate birthdate;
+    private final String email;
+    private final String phoneNumber;
+    private final String Speciality ;
 
+    public Teacher(int id, String firstName, String lastName, LocalDate birthdate, String email, String phoneNumber, String speciality) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthdate = birthdate;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        Speciality = speciality;
+    }
 }
