ecb0beeba566ce423b4ad3941bf4c58d3f88a492
antso.tech <hei.antso.2007@gmail.com>
2025-11-12 15:52:47 +0300

    modification de la structure de l'application

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,8 +12,8 @@ public class Main {
         Teacher Danielle = new Teacher(2,"John","Danielle",LocalDate.of(1996,5,8),"DanielleJohn@gmail.com","34 67 18 10 60","Front End");
         Tutor Elisa = new Tutor(1,"Bryar","Elisa",LocalDate.of(1980,12,8),"ElisaBryar04@gmail.com","11 14 05 78 20","mére de l'etudiant");
         Tutor Gabriel = new Tutor(1, "Francois","Gabriel",LocalDate.of(1985,6,12), "GabrielFrancois@gmail.com","33 77 78 89 67","Pere de l'étudiant");
-        Student Emily = new Student(1, "Bryar","Emily" ,LocalDate.of(2005,6,15),"emilyBryar.hei.4@gmail.com","24 79 89 90 70","F1",Elisa,);
-        Student Jean = new Student(2, "Francois","Jean", LocalDate.of(2004,9,18),"JeanFrancois14@gmail.com","14 69 17 05 89","F3",Gabriel, List.of());
+        Student Emily = new Student(1, "Bryar","Emily" ,LocalDate.of(2005,6,15),"emilyBryar.hei.4@gmail.com","24 79 89 90 70","F1",Elisa);
+        Student Jean = new Student(2, "Francois","Jean", LocalDate.of(2004,9,18),"JeanFrancois14@gmail.com","14 69 17 05 89","F3",Gabriel);
         Subject PROG1 = new Subject(1,Label.PROG1,8,James);
         Subject WEB1 = new Subject(2,Label.WEB1,6,Danielle);
         History history1 = new History(13, Instant.parse("2025-11-12T08:37:00Z"),"Pénalité aprés avoir non fini un devoir ");
--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -3,25 +3,16 @@ package org.example;
 import lombok.Getter;
 
 import java.time.LocalDate;
-import java.util.List;
 
 @Getter
 public class Student extends People{
     private final String group;
     private final Tutor tutor;
-    private final Exams exams;
 
-    public Student(int id, String firstName, String lastName, LocalDate birthdate, String email, String phoneNumber, String group, Tutor tutor, Exams exams) {
+    public Student(int id, String firstName, String lastName, LocalDate birthdate, String email, String phoneNumber, String group, Tutor tutor) {
         super(id, firstName, lastName, birthdate, email, phoneNumber);
         this.group = group;
         this.tutor = tutor;
-        this.exams = exams;
     }
 
-
-    public Integer finalNote (){
-        if(getExams().getNotes().getHistory().isEmpty()){
-            return getExams().getNotes().getInitialValue();
-        }
-    }
 }
