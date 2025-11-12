c9eb24b815491a1ec4f036056c01ebab0473e81b
antso.tech <hei.antso.2007@gmail.com>
2025-11-12 09:53:10 +0300

    ajout des attributs oubliéq

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,9 +11,9 @@ public class Main {
         Teacher James = new Teacher(1,"Frances","James", LocalDate.of(1990,11,6),"james@gmail.com","01 23 45 67 89","FullStack");
         Teacher Danielle = new Teacher(2,"John","Danielle",LocalDate.of(1996,5,8),"DanielleJohn@gmail.com","34 67 18 10 60","Front End");
         Tutor Elisa = new Tutor(1,"Bryar","Elisa","ElisaBryar04@gmail.com","11 14 05 78 20","mére de l'etudiant");
-        Tutor Gabriel = new Tutor(1, "Francois","Gabreil")
+        Tutor Gabriel = new Tutor(1, "Francois","Gabriel", "GabrielFrancois@gmail.com","33 77 78 89 67","Pere de l'étudiant")
         Student Emily = new Student(1, "Bryar", LocalDate.of(2005,6,15),"emilyBryar.hei.4@gmail.com","24 79 89 90 70","F1",Elisa);
-        Student Jean = new Student(2, "Francois","Jean", LocalDate.of(2004,9,18),"14 69 17 05 89","F3" ,);
+        Student Jean = new Student(2, "Francois","Jean", LocalDate.of(2004,9,18),"14 69 17 05 89","F3",Gabriel);
         Subject PROG1 = new Subject(1,Label.PROG1,8,James);
         Subject WEB1 = new Subject(2,Label.WEB1,6,Danielle);
         History history1 = new History(13, Instant.parse("2025-11-12T08:37:00Z"),"Pénalité aprés avoir non fini un devoir ");
--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -8,6 +8,7 @@ import java.util.List;
 @Getter
 public class Student {
     private final Integer id;
+    private final String lastName;
     private final String firstname;
     private final LocalDate BirthDate;
     private final String email;
@@ -16,8 +17,9 @@ public class Student {
     private final Tutor tutor;
     private List<Note> note;
 
-    public Student(Integer id, String firstname, LocalDate birthDate, String email, String phoneNumber, String group, Tutor tutor) {
+    public Student(Integer id,String lastName, String firstname, LocalDate birthDate, String email, String phoneNumber, String group, Tutor tutor) {
         this.id = id;
+        this.lastName = lastName;
         this.firstname = firstname;
         this.BirthDate = birthDate;
         this.email = email;
